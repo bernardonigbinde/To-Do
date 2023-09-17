d4103d4109947e263db22242aa59b3e8ca3f012f
Bernard Onigbinde <bernard.onigbinde@gmail.com>
2023-09-17 15:23:11 +0200

    Initial Commit

---
+++ To Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To Do
+//
+//  Created by Bernard Onigbinde on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
